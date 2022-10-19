ce0e410df192bfc84d13cddd5dea454d3dd1cf21
Flavio Nunes <flavioborgesnunes@gmail.com>
2022-10-19 18:01:57 -0300

    inicio animações

--- js/index.js
+++ js/index.js
@@ -17,14 +17,41 @@ window.onload = function init() {
     }
     else{
         $(document).ready(function(){
-        $('.carousel').slick({
-            dots: true,
-            arrows: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 1,
+            $('.carousel').slick({
+                dots: true,
+                arrows: true,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            });
         });
+    }
+    $(document).ready(function(){
+        $('nav').fadeTo(1,0)
+        $('nav').fadeTo(2000,1)
+        $(".secao-1").fadeTo(1,0);
+        $(".secao-1").fadeTo(500,1);
+
     });
-}
+    var $target = $('.anime'),
+        animationClass = 'anime-start',
+        offset = $(window).height() * 3/4;
+
+    function animeScroll(){
+        var documentTop = $(document).scrollTop();
+    
+        $target.each(function(){
+            var itemTop = $(this).offset().top;
+            if (documentTop > itemTop - offset){
+                $(this).addClass(animationClass);
+            }else{
+                $(this).removeClass(animationClass);
+            }
+        });
+    }
+    animeScroll();
+    $(document).scroll(function(){
+        animeScroll()
+    });
 };
