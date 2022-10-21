7da11a579e4fbab756026f1a73b7c46102857ee8
Flavio Nunes <flavioborgesnunes@gmail.com>
2022-10-21 15:34:34 -0300

    novas imagens

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ window.onload = function init() {
                 infinite: true,
                 speed: 500,
                 slidesToShow: 3,
-                slidesToScroll: 1,
+                slidesToScroll: 2,
             });
         });
     }
